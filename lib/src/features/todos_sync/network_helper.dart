92cc96e89d60ea976ccbfd1552bce3563bd28f1c
IBKnight <youtube.integral@mail.ru>
2024-07-11 07:58:16 +0300

    fixed

--- lib/src/features/todos_sync/network_helper.dart
+++ lib/src/features/todos_sync/network_helper.dart
@@ -1,17 +1,19 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:yandex_todo_list/src/core/database/db_service.dart';
 import 'package:yandex_todo_list/src/features/todos_sync/bloc/network_status_bloc.dart';
 
 class NetworkHelper {
   
+
   static void observeNetwork() {
-    Connectivity()
-        .onConnectivityChanged
-        .listen((List<ConnectivityResult> result) {
-      if (result.last == ConnectivityResult.none) {
-        NetworkStatusBloc().add(NetworkStatusNotify());
-      } else {
-        NetworkStatusBloc().add(NetworkStatusNotify(isConnected: true));
-      }
-    });
+    // Connectivity()
+    //     .onConnectivityChanged
+    //     .listen((List<ConnectivityResult> result) {
+    //   if (result.last == ConnectivityResult.none) {
+    //     NetworkStatusBloc().add(NetworkStatusNotify());
+    //   } else {
+    //     NetworkStatusBloc().add(NetworkStatusNotify(isConnected: true));
+    //   }
+    // });
   }
 }
